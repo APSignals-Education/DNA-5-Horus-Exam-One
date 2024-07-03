ab58091991abdea489b4f7c6ee37521bd4cc4c22
khashayaryaghma <khashayaryaghma@gmail.com>
2024-07-03 15:49:18 +0330

    edit

--- q-2.js
+++ q-2.js
@@ -1,7 +1,7 @@
 // ? // ? You are given an array of numbers (it's more than 2 numbers in that) you should write a function that returns only odd or even number in array
 //  ! Examples [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
 // ![160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
-
+// in each array exist just *one* odd or even value You should return that value in the function
 function findOutlier(integers) {
   //your code here
 }
